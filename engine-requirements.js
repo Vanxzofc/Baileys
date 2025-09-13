3fcc92e3a1e3fb334ea65838d3fa1599893b5b03
Vanxzofc <ivanmaulanaafandy8@gmail.com>
2025-09-13 18:56:23 +0700

    add engine-requirements.js

---
+++ engine-requirements.js
@@ -0,0 +1,9 @@
+const major = parseInt(process.versions.node.split('.')[0], 10)
+if (major < 20) {
+console.error(
+`\n❌ Paket ini membutuhkan Node.js versi 20 atau lebih tinggi.\n` +
+`   Saat ini kamu menggunakan Node.js versi ${process.versions.node}.\n` +
+`   Silakan perbarui ke Node.js 20 atau lebih tinggi untuk dapat menjalankan paket ini.\n`
+)
+process.exit(1)
+}
